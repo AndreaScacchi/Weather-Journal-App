c8e12759ffba5518cd401ed71591a732220a3c47
AndreaScacchi <69596559+AndreaScacchi@users.noreply.github.com>
2020-11-07 08:04:42 +0100

    Update app.js

--- Weather-Journal-App/Website/app.js
+++ Weather-Journal-App/Website/app.js
@@ -1,6 +1,6 @@
 // Global variables
-const baseURL = 'http://api.openweathermap.org/data/2.5/?zip=';
-const apiKey = '&appid=470a765db3a57bf10f903479bd62d114';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&appid=470a765db3a57bf10f903479bd62d114&units=metric';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -11,13 +11,24 @@ document.getElementById('generate').addEventListener('click', performAction)
 function performAction(e) {
     const zipCode = document.getElementById('zip').value;
     const feelings = document.getElementById('feelings').value;
+
+    // Input validation 
+    if(zipCode.length == 0) {
+        alert("Please enter zip code");
+        return
+    }
+    if(feelings.length == 0) {
+        alert("Please enter feelings");
+        return
+    }
+    
     getWeather(baseURL, zipCode, apiKey)
 
     .then(function(data) {
-        console.log(data);
+        console.log('this is data', data);
         postData('/add', {temperature: data.main.temp, date: newDate, feelings: feelings})
     })
-    .then(function(res) {
+    .then(function() {
         updateUI();
     })
 };
@@ -60,10 +71,10 @@ const updateUI = async () => {
     const request = await fetch('/all')
     try {
         const allData = await request.json()
-        console.log(allData);
-        document.getElementById('date').innerHTML = allData[0].date;
-        document.getElementById('temp').innerHTML = allData[0].temperature;
-        document.querySelector('#content').innerHTML = allData[0].feelings;
+        //console.log(allData);
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temperature;
+        document.querySelector('#content').innerHTML = allData.feelings;
     } catch(error) {
         console.log("error", error);
     }
